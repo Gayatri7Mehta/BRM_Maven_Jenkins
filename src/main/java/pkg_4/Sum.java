7f368dcfcb614664f3f6e47a4e6550b7afd86973
Gayatri <gayatri29mehta@gmail.com>
2023-02-18 21:44:10 +0530

    first commit

---
+++ src/main/java/pkg_4/Sum.java
@@ -0,0 +1,14 @@
+package pkg_4;
+
+public class Sum {
+	int add(int a, int b) {
+		int c= a+b;
+		return c;
+	}
+
+public static void main(String args[]) {
+	Sum obj= new Sum();
+	System.out.println(obj.add(6, 7));
+	
+}
+}
