23021c415802caa42ba3efb8ed7d9582b111d67a
Gayatri7Mehta <97516881+Gayatri7Mehta@users.noreply.github.com>
2023-10-24 10:08:20 +0530

    Update Sum.java

--- src/main/java/pkg_4/Sum.java
+++ src/main/java/pkg_4/Sum.java
@@ -1,24 +1,23 @@
 package pkg_4;
 
 public class Sum {
-	int add(int a, int b) {
-		int c= a+b;
-		return c;
-	}
-public class Sub{
-	int substract(int a,int b){
-		int d=a+b;
-		return d;
-	}
-
-public static void main(String args[]) {
-	Sum obj= new Sum();
-	Sub obj1=new Sub();
-	System.out.println(obj.add(6, 7));
-	System.out.println(obj.add(12, 5));
-	System.out.println(obj1.sub(6,4));
-
-	
-	
+    int add(int a, int b) {
+        int c = a + b;
+        return c;
+    }
 }
+
+class Sub {
+    int subtract(int a, int b) {
+        int d = a - b; 
+        return d;
+    }
+
+    public static void main(String args[]) {
+        Sum obj = new Sum();
+        Sub obj1 = new Sub();
+        System.out.println(obj.add(6, 7));
+        System.out.println(obj.add(12, 5));
+        System.out.println(obj1.subtract(6, 4)); 
+    }
 }
